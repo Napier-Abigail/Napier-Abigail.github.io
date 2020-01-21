6f331791b603b459ea533a1e9e0ca54bd617396a
Abigail Napier <abinapier@Abigails-MacBook-Pro-2.local>
2020-01-21 15:06:57 -0700

    ajax project

---
+++ AJAX.js
@@ -0,0 +1,19 @@
+function getFact(){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			let responseJSON = JSON.parse(this.responseText);
+			if(responseJSON.found==true){
+				document.getElementById("factOutput").innerHTML = responseJSON.text;
+			}else{
+				document.getElementById("factOutput").innerHTML = "No fact found for "+responseJSON.number;
+			}
+			
+		}
+	};
+	let usrNum = document.getElementById("usrNum").value;
+	let type = document.getElementById("type").options[document.getElementById("type").selectedIndex].value;
+	let url = "http://numbersapi.com/"+usrNum+"/"+type+"?json";
+	xhttp.open("GET", url, true);
+	xhttp.send();
+}
