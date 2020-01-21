76a52a1a034922e146a5122b981bc15240b0ef5d
Abigail Napier <abinapier@Abigails-MacBook-Pro-2.local>
2020-01-21 16:57:28 -0700

    js fix

--- AJAX.js
+++ AJAX.js
@@ -2,6 +2,7 @@ function getInfo(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
+			alert(this.responseText);
 			let responseJSON = JSON.parse(this.responseText);
 			alert(responseJSON);
 			let name = responseJSON.results.name;
