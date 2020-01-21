36ad53e9136a900157c789842d35051e3b3bb2ef
Abigail Napier <abinapier@Abigails-MacBook-Pro-2.local>
2020-01-21 16:45:41 -0700

    super

--- AJAX.js
+++ AJAX.js
@@ -1,19 +1,37 @@
-function getFact(){
+function getInfo(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			let responseJSON = JSON.parse(this.responseText);
-			if(responseJSON.found==true){
-				document.getElementById("factOutput").innerHTML = responseJSON.text;
-			}else{
-				document.getElementById("factOutput").innerHTML = "No fact found for "+responseJSON.number;
-			}
-			
+			let name = responseJSON.results.name;
+			let about = responeJSON.results.description;
+			let outputArea = document.getElementById("output");
+			outputArea.innerHTML="";
+			let htmlName = document.createElement("H4");
+			htmlName.innerHTML=name;
+			let htmlAbout = document.createElement("P");
+			htmlAbout.innerHTML=about;
+			outputArea.appendChild(htmlName);
+			outputArea.appendChild(htmlAbout);
 		}
 	};
-	let usrNum = document.getElementById("usrNum").value;
-	let type = document.getElementById("type").options[document.getElementById("type").selectedIndex].value;
-	let url = "http://numbersapi.com/"+usrNum+"/"+type+"?json";
+	let type = document.getElementById("hero").options[document.getElementById("hero").selectedIndex].value;
+	let apiKey ="3f07b9805231f32e863cdf1dd7450608";
+	let url = "https://gateway.marvel.com:443/v1/public/characters/";
+	switch(type) {
+		case "spiderman":
+		url+="1009610?apikey="+apiKey;
+		break;
+		case "capamerica":
+		url+="1009220?apikey="+apiKey;
+		break;
+		case "ironman":
+		url+="1009368?apikey="+apiKey;
+		break;
+		case "hulk":
+		url+="1009664?apikey="+apiKey;
+		break;
+	} 
 	xhttp.open("GET", url, true);
 	xhttp.send();
 }
