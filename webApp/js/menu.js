81461fca90be2f62e7c4541fc83220e53b99c186
Abigail Napier <abinapier@Abigails-MacBook-Pro-2.local>
2020-03-20 13:03:18 -0600

    mobile homepage

---
+++ webApp/js/menu.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+	document.getElementById("primaryNav").classList.toggle("hide");
+	document.getElementById("hamburger").classList.toggle("hide");
+	document.getElementById("close").classList.toggle("hide");
+}
