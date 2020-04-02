ea5fb44d0098df09bb51a333a7636ae041f23dbf
abinapier <abi.napier@yahoo.com>
2020-04-02 16:49:42 -0600

    finish app

--- webApp/js/menu.js
+++ webApp/js/menu.js
@@ -21,14 +21,12 @@ xhttp.open("GET", "https://api.unsplash.com/photos/random?client_id=NwVCbfHsJz3k
 xhttp.send();
 
 
+
 function timer(min){
     var sec = min*60;
     var timer = setInterval(function(){
         document.getElementById('timerDisplay').textContent =pad(Math.floor(sec/60))+':'+pad(sec%60);
         sec--;
-        if (sec < 0) {
-            clearInterval(timer);
-        }
         if (sec == 0){
         	document.getElementById('alert').play();
         }
@@ -39,5 +37,40 @@ function pad(number) {
    
      return (number < 10 ? '0' : '') + number
    
+}
+
+
+function getWeather(){
+  let zip = document.getElementById("zip").value;
+  if(zip.length>0){
+    let url = "https://api.openweathermap.org/data/2.5/weather?zip="+zip+",us&units=imperial&appid=6dda53eb2f27ab16803fca62cb290f2d";
+    var xhttp = new XMLHttpRequest(); 
+
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        let weatherObj = JSON.parse(this.responseText);
+        let temp = weatherObj.main.temp;
+        let current = weatherObj.weather[0].description;
+        let imgPath = 'https://openweathermap.org/img/wn/' + weatherObj.weather[0].icon+ '.png';
+        let weathertemp = document.createElement('p');
+        let weathericon = document.createElement('img');
+        let weathercurrent = document.createElement('p');
+        weathertemp.textContent = temp+"°F";
+        weathercurrent.textContent = current;
+        weathericon.src = imgPath;
+        weathericon.alt = current;
+        let outputArea = document.getElementById("output");
+        outputArea.innerHTML="";
+        outputArea.appendChild(weathertemp);
+        outputArea.appendChild(weathercurrent);
+        outputArea.appendChild(weathericon);
+      }else{
+        document.getElementById("output").innerHTML="Zip Code not found.";
+      }
+    }
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }
+  
 }
 
---
+++ webApp/js/task.js
@@ -0,0 +1,65 @@
+let input = document.getElementById("taskInput");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    addTask();
+  }
+}); 
+
+function addTask(){
+  if(input.value.length>0){
+    if (localStorage.getItem("tasks") === null) {
+      localStorage.setItem('tasks', input.value);
+    }else{
+      let myTasks = localStorage.getItem('tasks').split(",");
+      myTasks.push(input.value);
+      localStorage.setItem('tasks', myTasks.toString());
+    }
+  }
+  input.value = "";
+  updateTaskList();
+}
+
+function updateTaskList(){
+  if (localStorage.getItem("tasks")!= null){
+    let myTasks = localStorage.getItem("tasks").split(",");
+    let taskSection = document.getElementById("tasks");
+    if(taskSection.children.length>2){
+      let toDelete = taskSection.children.length-2;
+      for(let j=0; j<toDelete; j++){
+        taskSection.removeChild(taskSection.children[0]);
+      }
+    }
+    let insertPoint = document.getElementsByClassName("circle")[0];
+    for(let i=0; i<myTasks.length; i++){
+      let newCircle = insertPoint.cloneNode(deep=true);
+      let newTask = document.createElement("p");
+      newTask.textContent = myTasks[i];
+      taskSection.insertBefore(newCircle, insertPoint);
+      taskSection.insertBefore(newTask, insertPoint);
+    }
+  }
+}
+
+function removeItem(e){
+
+  var svg = e.currentTarget;
+  var circle = svg.children[0];
+  var paragraph = svg.nextSibling;
+  let text = paragraph.textContent;
+
+  let strikeText = document.createElement("s");
+  strikeText.textContent=text;
+  paragraph.textContent="";
+  strikeText.textContent = text;
+  paragraph.appendChild(strikeText);
+  circle.style.fill="black";
+  let myItems = localStorage.getItem("tasks").split(',');
+  for(let i = 0; i<myItems.length; i++){
+    if(myItems[i]==text){
+      myItems.splice(i, 1);
+    }
+  }
+  localStorage.setItem("tasks", myItems.toString());
+}
+
+updateTaskList();
